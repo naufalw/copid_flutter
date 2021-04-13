b66fc6d594c069fd04fd13b31a69a09d7abdb967
Naufal Wiwit Putra <67492436+naufalw@users.noreply.github.com>
2021-04-13 07:49:37 +0700

    ScreenUtil : initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:copid_flutter/constants.dart';
 import 'package:copid_flutter/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -13,13 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Copid App',
-      themeMode: ThemeMode.dark,
-      defaultTransition: Transition.zoom,
-      theme: klightTheme,
-      darkTheme: kdarkTheme,
-      home: MainScreen(),
+    return ScreenUtilInit(
+      designSize: Size(1440, 3040),
+      builder: () => GetMaterialApp(
+        title: 'Copid App',
+        themeMode: ThemeMode.dark,
+        defaultTransition: Transition.zoom,
+        theme: klightTheme,
+        darkTheme: kdarkTheme,
+        home: MainScreen(),
+      ),
     );
   }
 }
